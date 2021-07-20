40a6217274e07487c4bd1bbd2d4e53c2049f98b4
mnougerede <mnougerede@gmail.com>
2021-07-20 21:20:35 +0100

    added methods for Parser class. added placeholder direct file directory into main for testing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        Parser parser = new Parser(args[0]);
+        // Parser parser = new Parser(args[0]);
+        Parser parser = new Parser("C:\\Users\\mnoug\\Study\\Coursera\\Nand2Tetris\\source files\\nand2tetris\\projects\\06\\add\\Add.asm");
     }
 }
--- src/Parser.java
+++ src/Parser.java
@@ -1,6 +1,4 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Parser {
@@ -8,16 +6,34 @@ public class Parser {
     public Parser(String filename) {
         // opens the input file and gets ready to pass it
         //TODO build Parser constructor (opens file to be read)
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
     }
 
     private boolean hasMoreCommands() {
         //TODO build hasMoreCommands method
-        return true; //placeholder
+
     }
     private static void advance(){
         //TODO build advance method
     }
 
+    private String commandType() {
+        //TODO build commandType method
+    }
 
+    private String symbol() {
+        //TODO build symbol method
+    }
 
+    private String dest() {
+        //TODO build dest method
+    }
+
+    private String comp() {
+        //TODO build comp method
+    }
+
+    private String jump() {
+        //TODO build jump method
+    }
 }
